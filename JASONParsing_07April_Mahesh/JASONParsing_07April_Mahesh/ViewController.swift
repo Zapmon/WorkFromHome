b7d96db60458a305b67a73a48012fa9a2f659c2e
Mahesh <mahesh@Maheshs-Mac.local>
2020-04-07 17:03:39 +0530

    First commit

---
+++ JASONParsing_07April_Mahesh/JASONParsing_07April_Mahesh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JASONParsing_07April_Mahesh
+//
+//  Created by Mahesh on 07/04/20.
+//  Copyright © 2020 Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
