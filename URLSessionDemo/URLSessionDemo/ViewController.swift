abe641f4283d3f4c6fd5b63e3173a3678c87f05c
Mahesh <mahesh@Maheshs-Mac.local>
2020-04-13 14:15:33 +0530

    first commit

---
+++ URLSessionDemo/URLSessionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  URLSessionDemo
+//
+//  Created by Mahesh on 13/04/20.
+//  Copyright © 2020 Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
